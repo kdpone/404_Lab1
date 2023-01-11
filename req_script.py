a70d86f852f025cd8c3423be5e215b7889b9e0bf
kdpone <pausangklyde@gmail.com>
2023-01-11 14:54:30 -0700

    final script

--- req_script.py
+++ req_script.py
@@ -2,5 +2,11 @@ import requests as req
 
 print(req.__version__)
 
-r = req.get('https://www.google.com/')
-print(r)
+#r = req.get('https://www.google.com/')
+#print(r)
+
+raw_site = 'https://raw.githubusercontent.com/kdpone/404_Lab1/master/req_script.py?token=GHSAT0AAAAAAB4X6SKMSM6XVGVNK7TTLOHIY57FVZA'
+
+r = req.get(raw_site)
+
+open('downloaded_raw_code.py', 'wb').write(r.content)
